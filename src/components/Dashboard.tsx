ba680701ad97eb67bfd59276bed5d30c57138255
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-04-14 03:33:04 +0100

    added file upload dialog and endpoint

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UploadButton from "./UploadButton";
+import { trpc } from "@/app/_trpc/client";
+
+function Dashboard() {
+  const { data } = trpc.getUserFiles.useQuery();
+
+  return (
+    <main className="mx-auto max-w-7xl md:p-10">
+      <div className="mt-8 flex-col flex items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
+        <h1 className="mb-3 font-bold text-5xl text-gray-900">My files</h1>
+
+        <UploadButton />
+      </div>
+    </main>
+  );
+}
+
+export default Dashboard;
